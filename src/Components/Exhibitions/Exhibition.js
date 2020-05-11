04187da3806df8b5b27965196ce1c1755ba237cd
Lucas Alves Costa <lucas.alves.costa96@gmail.com>
2020-05-10 23:07:48 -0300

    Make sentences in italic

--- src/Components/Exhibitions/Exhibition.js
+++ src/Components/Exhibitions/Exhibition.js
@@ -166,8 +166,10 @@ function Exhibition (props) {
     thirdParagraph.current.innerHTML = thirdParagraph.current.innerHTML
     .replace(/Mata Atlântica \(Digital\)/g, `<i>Mata Atlântica (Digital)</i>`)
     .replace(/Mata Atlântica/g, `<i>Mata Atlântica</i>`)
+    .replace(/Superfície de Um lugar para estar/g, `<i>Superfície de Um lugar para estar</i>`)
     .replace(/Um lugar para estar/g, `<i>Um lugar para estar</i>`)
     .replace(/Superfície de Mundo Aberto/g, `<i>Superfície de Mundo Aberto</i>`)
+    .replace(/Perspectiva aérea de Mundo Aberto/g, `<i>Perspectiva aérea de Mundo Aberto</i>`)
     .replace(/Mundo Aberto/g, `<i>Mundo Aberto</i>`)
     .replace(/Terreno/g, `<i>Terreno</i>`)
     .replace(/Paraíso/g, `<i>Paraíso</i>`)
